98a809cbbb89f83bd64cc1a22b680980dd976a87
Joshua Arredondo <josh.arredondo92@gmail.com>
2019-03-01 07:05:38 -0800

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,14 +1,47 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
+var deleteBtns = document.getElementsByClassName("delete");
+var items = ul.getElementsByTagName("li");
+
+for (var i = 0; i < deleteBtns.length; i++) {
+  deleteBtns[i].addEventListener("click", removeParent);
+}
+
+function removeParent(evt) {
+  evt.target.parentNode.remove();
+}
+
+function getEventTarget(e) {
+  e = e || window.event;
+  return e.target || e.srcElement;
+}
+
+ul.onclick = function(event) {
+  var target = getEventTarget(event);
+  target.classList.toggle("done");
+}
+
+
+
+
+
+
+
 
 function inputLength() {
 	return input.value.length;
 }
 
 function createListElement() {
+  var btn = document.createElement("button");
+	btn.innerHTML = "Delete";
+	btn.onclick = removeParent;
 	var li = document.createElement("li");
 	li.appendChild(document.createTextNode(input.value));
+  li.innerHTML = li.innerHTML + " ";
+  li.appendChild(btn);
+
 	ul.appendChild(li);
 	input.value = "";
 }
